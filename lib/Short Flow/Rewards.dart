dc0b03d04b1485d7830ad4c5d797f6f0adc05500
Hurera Bhalli <hurerabhalli@gmail.com>
2024-04-25 22:20:47 +0500

    Updated

--- lib/Short Flow/Rewards.dart
+++ lib/Short Flow/Rewards.dart
@@ -62,29 +62,421 @@ class Reward extends StatelessWidget {
 
 
 
-      body: Column(
-        children: [
-         Padding(
-           padding: const EdgeInsets.symmetric(horizontal: 22.0),
-           child: Column(
-             children: [
-               const SizedBox(
-                 height: 30,
-                 width: double.infinity,
-               ),
-            // Image.asset('https://www.ymfcarparts.co.uk/images/blog/0-full-car-service.jpg'),
-               Container(
-                 height: 160,
-                 decoration: const BoxDecoration(
-                   color: Colors.white,
-                   borderRadius: BorderRadius.all(Radius.circular(14)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+           Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 22.0),
+             child: Column(
+               children: [
+                 const SizedBox(
+                   height: 30,
+                   width: double.infinity,
                  ),
-                 child: Image.asset('assets/pictures/Car.png'),
-               )
-             ],
+        
+        
+                 Container(
+                   height: 160,
+                   decoration: const BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.all(Radius.circular(14)),
+                   ),
+                   child: ClipRRect(
+                     borderRadius: BorderRadius.all(Radius.circular(14)),
+                     child: Image.asset(
+                       'assets/pictures/Car.png', fit: BoxFit.cover,),
+                   ),
+                 ),
+        
+                 const SizedBox(
+                   height: 10,
+                 ),
+                 const Row(
+                   children: [
+                     Text('Car wash', style: TextStyle(fontFamily: 'Poppins Medium', fontSize: 14, color: Color(0xff000000)),),
+                   ],
+                 ),
+        
+        
+                 const SizedBox(
+                   height: 3,
+                 ),
+        
+        
+                 const Row(
+                   children: [
+                     Text('Reward price:', style: TextStyle(fontFamily: 'Poppins Regular', fontSize: 12, color: Color(0xff000000)),),
+                     Spacer(flex: 1,),
+                     Text('\$34', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff6763EE)),),
+                   ],
+                 ),
+        
+        
+                 const SizedBox(
+                   height: 2,
+                 ),
+        
+        
+                 const Row(
+                   children: [
+                     Text('Date:', style: TextStyle(fontFamily: 'Poppins Regular', fontSize: 12, color: Color(0xff000000)),),
+                     Spacer(flex: 1,),
+                     Text('12 Jan, 2024', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff6763EE)),),
+                   ],
+                 ),
+        
+        
+        
+                 const SizedBox(
+                   height: 2,
+                 ),
+        
+        
+                 const Row(
+                   children: [
+                     Text('Expiry Date:', style: TextStyle(fontFamily: 'Poppins Regular', fontSize: 12, color: Color(0xff000000)),),
+                     Spacer(flex: 1,),
+                     Text('13 Mar, 2024', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff6763EE)),),
+                   ],
+                 ),
+        
+        
+                 const SizedBox(
+                   height: 10,
+                 ),
+        
+        
+        
+             Align(
+               alignment: Alignment.topRight,
+               child: GestureDetector(
+                 onTap: (){
+                   showDialog(context: context, builder: (context){
+                     return Padding(
+                       padding: const EdgeInsets.all(18.0),
+                       child: AlertDialog(
+                         shape: RoundedRectangleBorder(
+                           borderRadius: BorderRadius.circular(20),
+                         ),
+
+                         title: const Align(
+                           alignment: Alignment.center,
+                             child: Text('Scan QR Code', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff000000)),)),
+                         content: SvgPicture.asset('assets/icons/QRCode.svg'),
+                       ),
+                     );
+                   });
+                 },
+                 child: Container(
+                   height: 40,
+                   width: 100,
+                   decoration: BoxDecoration(
+                     gradient: LinearGradient(
+                       colors: [
+                         Color(0xff6763EE).withOpacity(0.7), // Red with 67% opacity
+                         Color(0xff6763EE).withOpacity(1), // Blue with 67% opacity
+                       ],
+                       begin: Alignment.topLeft,
+                       end: Alignment.bottomRight,
+                     ),
+
+                     borderRadius: BorderRadius.circular(8),
+                     // color: color,
+                   ),
+
+                   child: const Row(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: [
+                       Text('Claim', style: TextStyle(fontFamily: 'Poppins SemiBold', fontSize: 14, color: Colors.white),),
+                     ],
+                   ),
+                 ),
+               ),
+             ),
+        
+        
+        
+        
+                 SizedBox(
+                   height: 20,
+                 ),
+        
+                 //Second Picture
+        
+        
+                 Container(
+                   height: 160,
+                   decoration: const BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.all(Radius.circular(14)),
+                   ),
+                   child: ClipRRect(
+                     borderRadius: BorderRadius.all(Radius.circular(14)),
+                     child: Image.asset(
+                       'assets/pictures/Car.png', fit: BoxFit.cover,),
+                   ),
+                 ),
+        
+                 const SizedBox(
+                   height: 10,
+                 ),
+                 const Row(
+                   children: [
+                     Text('Car wash', style: TextStyle(fontFamily: 'Poppins Medium', fontSize: 14, color: Color(0xff000000)),),
+                   ],
+                 ),
+        
+        
+                 const SizedBox(
+                   height: 3,
+                 ),
+        
+        
+                 const Row(
+                   children: [
+                     Text('Reward price:', style: TextStyle(fontFamily: 'Poppins Regular', fontSize: 12, color: Color(0xff000000)),),
+                     Spacer(flex: 1,),
+                     Text('\$34', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff6763EE)),),
+                   ],
+                 ),
+        
+        
+                 const SizedBox(
+                   height: 2,
+                 ),
+        
+        
+                 const Row(
+                   children: [
+                     Text('Date:', style: TextStyle(fontFamily: 'Poppins Regular', fontSize: 12, color: Color(0xff000000)),),
+                     Spacer(flex: 1,),
+                     Text('12 Jan, 2024', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff6763EE)),),
+                   ],
+                 ),
+        
+        
+        
+                 const SizedBox(
+                   height: 2,
+                 ),
+        
+        
+                 const Row(
+                   children: [
+                     Text('Expiry Date:', style: TextStyle(fontFamily: 'Poppins Regular', fontSize: 12, color: Color(0xff000000)),),
+                     Spacer(flex: 1,),
+                     Text('13 Mar, 2024', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff6763EE)),),
+                   ],
+                 ),
+        
+        
+                 const SizedBox(
+                   height: 10,
+                 ),
+
+
+
+                 Align(
+                   alignment: Alignment.topRight,
+                   child: GestureDetector(
+                     onTap: (){
+                       showDialog(context: context, builder: (context){
+                         return Padding(
+                           padding: const EdgeInsets.all(18.0),
+                           child: AlertDialog(
+                             shape: RoundedRectangleBorder(
+                               borderRadius: BorderRadius.circular(20),
+                             ),
+
+                             title: const Align(
+                                 alignment: Alignment.center,
+                                 child: Text('Scan QR Code', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff000000)),)),
+                             content: SvgPicture.asset('assets/icons/QRCode.svg'),
+                           ),
+                         );
+                       });
+                     },
+                     child: Container(
+                       height: 40,
+                       width: 100,
+                       decoration: BoxDecoration(
+                         gradient: LinearGradient(
+                           colors: [
+                             Color(0xff6763EE).withOpacity(0.7), // Red with 67% opacity
+                             Color(0xff6763EE).withOpacity(1), // Blue with 67% opacity
+                           ],
+                           begin: Alignment.topLeft,
+                           end: Alignment.bottomRight,
+                         ),
+
+                         borderRadius: BorderRadius.circular(8),
+                         // color: color,
+                       ),
+
+                       child: const Row(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                         children: [
+                           Text('Claim', style: TextStyle(fontFamily: 'Poppins SemiBold', fontSize: 14, color: Colors.white),),
+                         ],
+                       ),
+                     ),
+                   ),
+                 ),
+
+
+
+
+
+
+
+                 //Third Picture
+        
+        
+        
+                 const SizedBox(
+                   height: 20,
+                 ),
+                 
+        
+        
+                 Container(
+                   height: 160,
+                   decoration: const BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.all(Radius.circular(14)),
+                   ),
+                   child: ClipRRect(
+                     borderRadius: BorderRadius.all(Radius.circular(14)),
+                     child: Image.asset(
+                       'assets/pictures/Car.png', fit: BoxFit.cover,),
+                   ),
+                 ),
+        
+                 const SizedBox(
+                   height: 10,
+                 ),
+                 const Row(
+                   children: [
+                     Text('Car wash', style: TextStyle(fontFamily: 'Poppins Medium', fontSize: 14, color: Color(0xff000000)),),
+                   ],
+                 ),
+        
+        
+                 const SizedBox(
+                   height: 3,
+                 ),
+        
+        
+                 const Row(
+                   children: [
+                     Text('Reward price:', style: TextStyle(fontFamily: 'Poppins Regular', fontSize: 12, color: Color(0xff000000)),),
+                     Spacer(flex: 1,),
+                     Text('\$34', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff6763EE)),),
+                   ],
+                 ),
+        
+        
+                 const SizedBox(
+                   height: 2,
+                 ),
+        
+        
+                 const Row(
+                   children: [
+                     Text('Date:', style: TextStyle(fontFamily: 'Poppins Regular', fontSize: 12, color: Color(0xff000000)),),
+                     Spacer(flex: 1,),
+                     Text('12 Jan, 2024', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff6763EE)),),
+                   ],
+                 ),
+        
+        
+        
+                 const SizedBox(
+                   height: 2,
+                 ),
+        
+        
+                 const Row(
+                   children: [
+                     Text('Expiry Date:', style: TextStyle(fontFamily: 'Poppins Regular', fontSize: 12, color: Color(0xff000000)),),
+                     Spacer(flex: 1,),
+                     Text('13 Mar, 2024', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff6763EE)),),
+                   ],
+                 ),
+        
+        
+                 const SizedBox(
+                   height: 10,
+                 ),
+
+
+
+                 Align(
+                   alignment: Alignment.topRight,
+                   child: GestureDetector(
+                     onTap: (){
+                       showDialog(context: context, builder: (context){
+                         return Padding(
+                           padding: const EdgeInsets.all(18.0),
+                           child: AlertDialog(
+                             shape: RoundedRectangleBorder(
+                               borderRadius: BorderRadius.circular(20),
+                             ),
+
+                             title: const Align(
+                                 alignment: Alignment.center,
+                                 child: Text('Scan QR Code', style: TextStyle(fontFamily: 'Poppins Semibold', fontSize: 14, color: Color(0xff000000)),)),
+                             content: SvgPicture.asset('assets/icons/QRCode.svg'),
+                           ),
+                         );
+                       });
+                     },
+                     child: Container(
+                       height: 40,
+                       width: 100,
+                       decoration: BoxDecoration(
+                         gradient: LinearGradient(
+                           colors: [
+                             Color(0xff6763EE).withOpacity(0.7), // Red with 67% opacity
+                             Color(0xff6763EE).withOpacity(1), // Blue with 67% opacity
+                           ],
+                           begin: Alignment.topLeft,
+                           end: Alignment.bottomRight,
+                         ),
+
+                         borderRadius: BorderRadius.circular(8),
+                         // color: color,
+                       ),
+
+                       child: const Row(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                         children: [
+                           Text('Claim', style: TextStyle(fontFamily: 'Poppins SemiBold', fontSize: 14, color: Colors.white),),
+                         ],
+                       ),
+                     ),
+                   ),
+                 ),
+
+
+
+
+
+
+                 const SizedBox(
+                   height: 40,
+                 ),
+
+
+
+
+
+
+               ],
+             ),
            ),
-         ),
-        ],
+          ],
+        ),
       ),
 
     );
